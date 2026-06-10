31a931fa5b5e8f97c34dc82bef8682c709e9db5e
Alejandro Mejía <alejandro.hernandez@atomchat.io>
2026-06-09 22:32:20 -0300

    chore(deps): add @angular/animations for snackbar support
    Enable provideAnimationsAsync so bulk-delete feedback can use AtomSnackbarService.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { provideDesignSystemIcons } from '@atomchat-io/ui-design-system';
@@ -12,6 +13,7 @@ import { appRoutes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
+    provideAnimationsAsync(),
     provideDesignSystemIcons(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes, withComponentInputBinding()),
